0695b05399bccf5a68c00b64c351bab2b04e7622
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2017-12-21 17:42:35 +0100

    change in js script

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var a = 6;
+var h = 6;
+var triangleArea;
+
+triangleArea = a*h/2 ;
+
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
+
