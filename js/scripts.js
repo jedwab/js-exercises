8833e4dbc8b2897402ff2e8ffc549279b47c1502
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2017-12-25 16:35:21 +0100

    add if + shortif

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 var a = 3;
-var b = 1;
+var b = 9;
 var value;
 
 value = (a * a) - (2 * a * b) + (b * b);
 
 console.log(value);
 
+if ( value == 0) {
+	console.log('wynik wynosi zero')
+
+} else {
  (value > 0) ? console.log('wynik dodatni') : console.log('wynik ujemny')
+}
+
 
 	
 
