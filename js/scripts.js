61313ec8a4ef97175c1dc8163fd18384528b9b68
jedwab <34577105+jedwab@users.noreply.github.com>
2017-12-27 21:15:39 +0100

    clean code

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,10 @@ value = (a * a) - (2 * a * b) + (b * b);
 
 console.log(value);
 
-if ( value == 0) {
-	console.log('wynik wynosi zero')
-
+if (value == 0) {
+	console.log('wynik wynosi zero');
 } else {
- (value > 0) ? console.log('wynik dodatni') : console.log('wynik ujemny')
+ (value > 0) ? console.log('wynik dodatni') : console.log('wynik ujemny');
 }
 
 
