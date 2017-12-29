3ceb65668e58d8f273b90a9af5c02a86e9cb8078
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2017-12-29 15:00:45 +0100

    exercise with strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
- 
-console.log(allNames);
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+dinosaur.toUpperCase();
 
-var newName = 'Marian';
+var textAfter = text.replace('Velociraptor', dinosaur.toUpperCase());
 
-if (allNames.indexOf(newName) === -1) {
-	allNames.push('Marian');
-}
+var halfText = textAfter.length/2;
+
+var expectedText = textAfter.slice(0, halfText);
+console.log(expectedText);
+
+
+
+
