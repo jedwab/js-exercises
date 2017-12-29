07c6c7860cc03f9aab5f49e327899b7f050ed334
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2017-12-29 11:31:49 +0100

    clean code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-var text = 'BeTTer THAN neveR';
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+ 
+console.log(allNames);
 
-if ( text.toLowerCase().indexOf('late') != -1 ) {
-    console.log('Sentence contains word - late')
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+	allNames.push('Marian');
 }
