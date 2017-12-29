27a9a99554dcad1aaafa7a63eca5cbb260fc5845
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2017-12-29 11:26:08 +0100

    clear code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 function getTriangleArea(a, h) {
-
-if ((a <= 0) || (h <= 0)) {
-		console.log('Nieprawidłowe dane');
-	}
-return a*h/2;
-}
+	if ((a <= 0) || (h <= 0)) {
+ 		return('Nieprawidłowe dane');
+ 	}
+ 	return a*h/2;
+ }
 console.log(getTriangleArea(10, 6));
-
+ 
 var triangleArea = getTriangleArea(10, 15);
 console.log(triangleArea);
-
+ 
 var triangleArea = getTriangleArea(6, 6);
 console.log(triangleArea);
-
+ 
 var triangleArea = getTriangleArea(11, 8);
 console.log(triangleArea);
-
+ 
 var triangleArea = getTriangleArea(0, 8);
-console.log(triangleArea);
+console.log(triangleArea); 
