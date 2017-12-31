6f40dc878944e0f6977a6a605ef3c341db3541d7
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2017-12-31 21:10:42 +0100

    add array

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 var dinosaur = 'triceratops';
 dinosaur.toUpperCase();
 
-var textAfter = text.replace('Velociraptor', dinosaur.toUpperCase());
+var textArray = text.split(" ");
+var foo = textArray[0];
 
+var textAfter = text.replace(foo, dinosaur.toUpperCase());
 var halfText = textAfter.length/2;
 
 var expectedText = textAfter.slice(0, halfText);
